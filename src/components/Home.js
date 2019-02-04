06583dd956349f9884410a67fcb65ada58c1d624
Begona Fernandez <boadicea1979@yahoo.es>
2019-02-04 12:02:41 +0000

    added https

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,12 +22,12 @@ class Home extends React.Component {
   }
 
   componentDidMount(){
-    axios.get(`http://www.omdbapi.com/?s=star&apikey=${apikey}`)
+    axios.get(`https://www.omdbapi.com/?s=star&apikey=${apikey}`)
       .then(res => this.setState({results: res.data.Search}))
   }
 
   apiCall() {
-    axios.get(`http://www.omdbapi.com/?s=${this.state.search}&type=movie&apikey=${apikey}`)
+    axios.get(`https://www.omdbapi.com/?s=${this.state.search}&type=movie&apikey=${apikey}`)
       .then(res => {
         if(res.data.Response === 'True') this.setState({results: res.data.Search, error: '', searched: this.state.search})
         else this.setState({results: [], error: res.data.Error, searched: this.state.search})
